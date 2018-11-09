af2eabe5ceaec849145e9ad7735fab3a89d31d26
Jesus Sarmiento <jsarmiento1614@gmail.com>
2018-11-09 13:11:26 -0600

    En este commit he intentado trabajarcon multer

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ var sql = require('mssql');
 var cors = require('cors');
 var bodyparser = require('body-parser');
 var env = require('dotenv');
+var multer = require('multer');
+var path = require('path');
+
+// Programo para que el servido me le cambie la extension a los archivos subidos.
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now()+'.'+getExtension(file.originalname))
+    }
+})
+
+function getExtension(filename) {
+    var ext = path.extname(filename||'').split('.');
+    return ext[ext.length - 1];
+}
+
+var upload = multer({ storage: storage })
+
+
 
 // Almaceno toda la funcionalidad del espress en la variable app.
 var app = express();
@@ -131,3 +152,67 @@ app.get('/app/v1/student', (req, res, next) =>{
 });
 
 
+//Creo funcion get para que los Maestros pueden marcar asistencia de un alumnos a una clase en particular.
+app.post('/app/v1/record/student', (req, res, next) =>{
+    // Obtendre del querystring el parametro de busqueda del usuario.
+    var assistance_date = req.body.assistance_date;
+    var assistance_checked = req.body.assistance_checked;
+    var class_ID = req.body.class_ID;
+
+    // Comprobamos que los datos se hayan enviado
+    if(!assistance_date && !assistance_checked && !class_ID){
+        res.send("<h1>Sucedio un error con tus datos.</h1>");
+    }
+
+    // Ejecuto la consulta a la base de datos
+    sql.connect(sqlconfig).then(() => {
+        return sql.query(`insert into dbo.Assistance(assistance_date,assistance_checked, class_ID) values('${assistance_date}', '${assistance_checked}',${class_ID});`);
+    }).then(result => {
+        var data = {
+            seccess: true,
+            message: `Se ha creado ${result.rowsAffected} registro nuevo`
+        }
+        res.send(data);
+
+        // cerrare la conexion.
+        sql.close();
+    }).catch(err => {
+        return next(err);
+    });
+});
+
+
+//Creo funcion get para que el administrador puede crear nuevos estudiantes
+app.post('/app/v1/student/new', upload.single('file'), (req, res, next) =>{
+    // Obtendre del querystring el parametro de busqueda del usuario y la imagen.
+    var code = req.body.code;
+    var student_Name = req.body.student_Name;
+    var fileName = req.file != null ? req.file.fileName : 'n/a';
+
+    // if (req.file == undefined) {
+    //     fileName = 'n/a';
+    //    } else {
+    //     fileName = req.file.filename;
+    // }
+
+    // Comprobamos que los datos se hayan enviado
+    if(!code && !student_Name){
+        res.send("<h1>Sucedio un error con tus datos.</h1> " +code + student_Name+fileName);
+    }
+
+    // Ejecuto la consulta a la base de datos
+    sql.connect(sqlconfig).then(() => {
+        return sql.query(`insert into dbo.Student(code, student_name, student_picture) values(${code}, '${student_Name}', '${fileName}');`);
+    }).then(result => {
+        var data = {
+            seccess: true,
+            message: `Se ha creado ${result.rowsAffected} registro nuevo`
+        }
+        res.send(data);
+
+        // cerrare la conexion.
+        sql.close();
+    }).catch(err => {
+        return next(err);
+    });
+});
